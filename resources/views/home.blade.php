4d1d934929aad4245360698d50c4906d1b425022
Allan Q <bolunqi@hotmail.com>
2019-09-05 16:37:20 -0700

    row bug fix

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,6 +13,37 @@
         <!-- Add Pagination -->
         <div class="swiper-pagination"></div>
     </div>
+    <div class="congratulation-container text-center">
+        <h4>Congratulations</h4>
+        <h4>恭喜 Gao Yawen 同学被西安大略 (UWO) 工程和科学系录取</h4>
+    </div>
+    <div class="prosperous-services-container">
+        <div class="prosperous-services-title text-center">
+            <h1>成就梦想 大展宏图</h1>
+            <h5>/ REALIZE YOUR AMBITION /</h5>
+            <div class="prosperous-services-title-wrap"><h4>PROSPEROUS</h4></div>
+        </div>
+        <div class="prosperous-services-box row no-gutters">
+            <div class="prosperous-services-box-wrap col-offset-1 col-md-10">
+                <div class="row">
+                    <div class="col-md-8">
+
+                    </div>
+                    <div class="col-md-4">
+
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-md-8">
+
+                    </div>
+                    <div class="col-md-4">
+
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
 @stop
 

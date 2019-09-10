fcb7a6a3b2238b677e3a814a53ea2710c19562ce
Allan Q <bolunqi@hotmail.com>
2019-09-09 22:11:55 -0700

    webpack bug fixed

--- resources/views/summer_camp.blade.php
+++ resources/views/summer_camp.blade.php
@@ -1,6 +1,22 @@
 @extends('layout.layout')
 @section('title','Summer Camp')
 
-@section('content')
+@section('css')
+@stop
 
+@section('content')
+<div id="summer-camp">
+    <div id="banner-main-block" class="section position-relative">
+        <img id="banner-bg" class="w-100 position-relative" src="{{asset('images/about_us/bg-gallery.png')}}" alt="image">
+        <img id="banner-main" class="position-absolute" src="{{asset('images/feature/banner-main-feature.png')}}" alt="banner">
+
+        <div class="info-slide-block">
+
+        </div>
+    </div>
+    @include('layout._congratulation')
+</div>
+@stop
+
+@section('js')
 @stop

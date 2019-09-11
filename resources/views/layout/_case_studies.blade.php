c2d60cd36689964986a97bd089af645518516297
Jason Li <jklsh0371@gmail.com>
2019-09-10 20:50:10 -0700

    jason

--- resources/views/layout/_case_studies.blade.php
+++ resources/views/layout/_case_studies.blade.php
@@ -1,4 +1,4 @@
-<div class="fourth text-center">
+<div class="case-study text-center">
     <div class="container">
         <div class="tit public w-100 d-block mt-5 mb-3 center">
             <span class="tit-y d-inline-block pl-4">
@@ -8,5 +8,33 @@
         </div>
         <h3>我们已为5000余名留学生开启了留学旅程</h3>
         <p>经过 15 年的辛勤耕耘，宏图教育留学团队已成功帮助数万名留学生完成了留学梦想，帮助一个个萃萃学子抒写精彩的留学故事．成功案例让你和他们一样</p>
+    </div>
+</div>
+
+<div class="case text-center">
+    <div class="container">
+        <div class="row">
+            <div class="col-special-center">
+                <img src="../images/application/ico-figure.png">
+                <p>该生于 2016 年 9 月入读加拿BC省9 年级，成绩一直不太理想。在万分沮丧的时候，家长来远播寻求帮助，我们的老师为她推荐了 20 17 暑假回国读日匕 C 课程，学生在暑期报读了数学 10 学分课，历时 50 课时，最终周同学以 95 ％的优异成绩通过学分课程， 1个月的时间，学生竟然实现了质的飞跃！家长连连称赞，在远播学习是正确的选择！</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col col-lg-1 offset-lg-1 padding-col"></div>
+            @for ($i = 0 ; $i<8; $i++)
+            <div class="col col-3 col-md-3 col-lg-1">
+                <img src="../images/application/ico-figure.png" class="w-100">
+            </div>
+            @endfor
+        </div>
+
+        <div class="row">
+            <div class="col col-lg-1 offset-lg-1 padding-col"></div>
+            @for ($i = 0 ; $i<8; $i++)
+            <div class="col col-3 col-md-3 col-lg-1">
+                <img src="../images/application/ico-figure.png" class="w-100">
+            </div>
+            @endfor
+        </div>
     </div>
 </div>
